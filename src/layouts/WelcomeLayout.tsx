1689d525c183dd98463dcc3a5749854356cffde8
Zhoujia22 <827160082@qq.com>
2023-05-05 22:24:02 +0800

    添加过渡动画

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,7 +1,23 @@
-import { Outlet } from 'react-router-dom'
+import { animated, useTransition } from '@react-spring/web'
+import type { ReactNode } from 'react'
+import { useLocation, useOutlet } from 'react-router-dom'
 
+const map: Record<string, ReactNode> = {}
 export const WelcomeLayout: React.FC = () => {
-  return (
-    <div><Outlet /> </div>
-  )
+  const location = useLocation() // 获取当前地址栏状态
+  const outlet = useOutlet()
+  map[location.pathname] = outlet
+  const transitions = useTransition(location.pathname, {
+    from: { transform: 'translateX(100%)' },
+    enter: { transform: 'translateX(0%)' },
+    leave: { transform: 'translateX(-100%)' },
+    config: { duration: 5000 },
+  })
+  return transitions((style, pathname) => {
+    return <animated.div key={pathname} style={style}>
+      <div style={{ textAlign: 'center' }}>
+        {map[pathname]}
+      </div>
+    </animated.div>
+  })
 }
