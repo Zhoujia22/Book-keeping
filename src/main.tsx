e3a2fb6ea1af1293740adefdf0639c34269d1063
Zhoujia22 <827160082@qq.com>
2023-05-05 20:14:10 +0800

    引入react-spring

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { ErrorPage } from './components/ErrorPage'
+import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RedirectToWelcome1 } from './components/ RedirectToWelcome1'
+import { Demo } from './components/Demo'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>root</div>,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/1',
-    element: <div>1</div>,
+    element: <Outlet />,
+    errorElement: <RedirectToWelcome1 />,
+    children: [
+      { index: true, element: <div>空1</div> },
+      {
+        path: 'welcome',
+        element: <Outlet />,
+        children: [
+          { index: true, element: <div>空2</div> },
+          { path: '1', element: <Demo></Demo> },
+          { path: '2', element: <div>2<NavLink to='/welcome/3'>下一页</NavLink></div> },
+          { path: '3', element: <div>3<NavLink to='/welcome/4'>下一页</NavLink></div> },
+          { path: '4', element: <div>4<NavLink to='/welcome/xxx'>开始记账</NavLink></div> },
+        ],
+      },
+    ],
   },
 ])
 
