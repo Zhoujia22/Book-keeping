3647c6a92e1b1c2878f1d07a9283cfe11fdee635
Zhoujia22 <827160082@qq.com>
2023-05-05 20:59:48 +0800

    改变目录结构

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { RedirectToWelcome1 } from './components/ RedirectToWelcome1'
-import { Demo } from './components/Demo'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Outlet />,
-    errorElement: <RedirectToWelcome1 />,
-    children: [
-      { index: true, element: <div>空1</div> },
-      {
-        path: 'welcome',
-        element: <Outlet />,
-        children: [
-          { index: true, element: <div>空2</div> },
-          { path: '1', element: <Demo></Demo> },
-          { path: '2', element: <div>2<NavLink to='/welcome/3'>下一页</NavLink></div> },
-          { path: '3', element: <div>3<NavLink to='/welcome/4'>下一页</NavLink></div> },
-          { path: '4', element: <div>4<NavLink to='/welcome/xxx'>开始记账</NavLink></div> },
-        ],
-      },
-    ],
-  },
-])
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes/router'
 
 const div = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(div)
