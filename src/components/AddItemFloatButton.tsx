c316009f19cdd21e93c394da480f77e571f1fd62
Zhoujia22 <827160082@qq.com>
2023-05-16 02:51:37 +0800

    引入 svgsprites Vite 插件

--- src/components/AddItemFloatButton.tsx
+++ src/components/AddItemFloatButton.tsx
@@ -1,9 +1,9 @@
-import add from '../assets/icons/add.svg'
-
 export const AddItemFloatButton: React.FC = () => {
   return (
-        <button b-none w-56px h-56px rounded="50%" bg='#ffd103' p-4px text-6xl fixed bottom-16px right-16px >
-            <img max-w="100%" max-h="100%" src={add} />
+        <button b-none w-56px h-56px rounded="50%" bg='#ffd103' p-4px  fixed bottom-16px right-16px >
+            <svg style={{ fill: 'white', width: '1.2em', height: '1.2em' }}>
+                <use xlinkHref="#add" ></use>
+            </svg>
         </button>
   )
 }
