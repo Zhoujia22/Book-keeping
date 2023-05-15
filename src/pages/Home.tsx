e1bf36f88ea430a377450f9774fb8a70d06eaf31
Zhoujia22 <827160082@qq.com>
2023-05-16 02:31:38 +0800

    新建三个组件 Loading.tsx AddItemFloatButton.tsx Icon.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import useSWR from 'swr'
 import { Navigate } from 'react-router-dom'
 import p from '../assets/images/logo.svg'
-import add from '../assets/icons/add.svg'
 import { ajax } from '../lib/ajax'
 import { useTitle } from '../hooks/useTitle'
+import { Loading } from '../components/Loading'
+import { AddItemFloatButton } from '../components/AddItemFloatButton'
 
 interface Props {
   title?: string
@@ -22,7 +23,7 @@ export const Home: React.FC<Props> = (props) => {
   const isLoadingItems = meData && !itemsData && !itemsError
 
   if (isLoadingMe || isLoadingItems) {
-    return <div>加载中</div>
+    return <Loading />
   }
 
   if (itemsData?.resources[0]) {
@@ -37,9 +38,7 @@ export const Home: React.FC<Props> = (props) => {
       <div px-16px >
         <button text-18px h-48px w="100%" bg='#ffd103' b-none text-black rounded-8px >开始记账</button>
       </div>
-      <button b-none w-56px h-56px rounded="50%" bg='#ffd103' p-4px text-6xl fixed bottom-16px right-16px >
-        <img max-w="100%" max-h="100%" src={add} />
-      </button>
+      <AddItemFloatButton />
     </div>
   )
 }
