6d9f2de01e70c9de71ac15c18521f4003a5f51eb
Zhoujia22 <827160082@qq.com>
2023-06-04 03:37:26 +0800

    给菜单添加滑动动画

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -47,7 +47,7 @@ export const ItemsPage: React.FC = () => {
       <ItemsSummary />
       <ItemsList items={items} />
       <AddItemFloatButton />
-      {visible ? <TopMenu onClickMask={() => setVisible(false)} /> : null}
+     <TopMenu onClickMask={() => setVisible(false)} visible={visible} />
     </div>
   )
 }
