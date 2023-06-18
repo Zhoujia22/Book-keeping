57e89bb2de1ba095e9dd235c990ca21a4439a494
Zhoujia22 <827160082@qq.com>
2023-06-19 03:51:45 +0800

    Tabs 支持自定义三处的样式

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,23 +1,34 @@
+import type { ReactNode } from 'react'
+import cs from 'classnames'
 import s from './Tabs.module.scss'
 
 type Props<T> = {
   tabItems: {
     key: T
     text: string
+    element?: ReactNode
   }[]
   value: T
   onChange: (key: T) => void
   className?: string
+  classPrefix?: string
 }
 
 export function Tabs<T extends string>(props: Props<T>) {
-  const { tabItems, value, onChange, className } = props
+  const { tabItems, value, onChange, className, classPrefix } = props
   return (
-    <ol className={className} flex children-px-24px children-py-12px >
-      {tabItems.map(item => <li key={item.key} className={item.key === value ? s.selected : ''}
-        onClick={() => onChange(item.key)}>
-        {item.text}
-      </li>)}
-    </ol>
+    <div className={cs(className, classPrefix)} >
+      <ol className={classPrefix ? `${classPrefix}-menu` : ''} flex children-px-24px children-py-12px bg='[rgb(255,232,37)]' >
+        {tabItems.map(item => <li key={item.key}
+          className={cs(item.key === value ? s.selected : '',
+            classPrefix ? `${classPrefix}-menu-item` : '')}
+          onClick={() => onChange(item.key)}>
+          {item.text}
+        </li>)}
+      </ol>
+      <div className={classPrefix ? `${classPrefix}-pane` : ''}>
+        {tabItems.filter(item => item.key === value)[0].element}
+      </div>
+    </div>
   )
 }
