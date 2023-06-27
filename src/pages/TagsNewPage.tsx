1d2d695e2bd5e5e36124489f320df8767067627e
Zhoujia22 <827160082@qq.com>
2023-06-28 05:08:12 +0800

    初步展示新建标签页面

--- src/pages/TagsNewPage.tsx
+++ src/pages/TagsNewPage.tsx
@@ -1,5 +1,40 @@
+import { useState } from 'react'
+import { emojis } from '../lib/emojis'
+
 export const TagsNewPage: React.FC = () => {
+  const onSubmit = () => { }
+  const [emojiKind, setEmojiKind] = useState('表情')
   return (
-    <div>TagsNewPage </div>
+        <div>
+            <form onSubmit={onSubmit}>
+                <div>
+                    <span>标签名</span>
+                    <input />
+                    <span text-red>标签名太长</span>
+                </div>
+                <div>
+                    <span>符号</span>
+                    <div>
+                        <div>
+                            {emojis.map((emoji) => <span key={emoji.name}
+                            onClick={() => setEmojiKind(emoji.name)}>{emoji.name}
+                            </span>)}
+                        </div>
+                        <div>
+                            {emojis.map((emoji) =>
+                                <div style={{ display: emoji.name === emojiKind ? '' : 'none' }}>
+                                    {emoji.chars}
+                                </div>)
+                            }
+                        </div>
+                    </div>
+                </div>
+                <p>记账时长按标签，即可进行编辑</p>
+                <div>
+                    <button j-btn>确定</button>
+                </div>
+            </form>
+
+        </div>
   )
 }
