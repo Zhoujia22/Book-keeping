f729e49fc536363099ba3e928418255b95d47f72
Zhoujia22 <827160082@qq.com>
2023-07-15 06:30:13 +0800

    可以倒计时了

--- src/components/Input/SmsCodeInput.tsx
+++ src/components/Input/SmsCodeInput.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 type Props = {
   value?: string
   placeholder?: string
@@ -7,15 +9,35 @@ type Props = {
 
 export const SmsCodeInput: React.FC<Props> = (props) => {
   const { value, placeholder, onChange, request } = props
+  const [started, setStarted] = useState(false)
+  const [count, setCount] = useState(5)
   const onClick = async () => {
     if (!request) { return }
     await request()
+    setStarted(true)
   }
+
+  useEffect(() => {
+    if (!started) { return }
+    if (count === -1) {
+      setStarted(false)
+      setCount(5)
+      return
+    }
+    const timer = setTimeout(() => {
+      setCount(count - 1)
+    }, 1000)
+    return () => clearTimeout(timer)
+  }, [started, count])
+
   return (
-        <div flex gap-x-16px>
-            <input shrink-1 max-w="[calc(40%-8px)]" j-input-text type="text" placeholder={placeholder}
-                value={value} onChange={e => onChange?.(e.target.value)} />
-            <button type='button' shrink-0 max-w='[calc(60%-8px)]' j-btn onClick={onClick}>发送验证码</button>
-        </div>
+    <div flex gap-x-16px>
+      <input shrink-1 max-w="[calc(40%-8px)]" j-input-text type="text" placeholder={placeholder}
+        value={value} onChange={e => onChange?.(e.target.value)} />
+      {started
+        ? <button type="button" max-w="[calc(60%-8px)]" shrink-0 j-btn>{count}</button>
+        : <button type="button" max-w="[calc(60%-8px)]" shrink-0 j-btn onClick={onClick}>发送验证码</button>
+      }
+    </div>
   )
 }
