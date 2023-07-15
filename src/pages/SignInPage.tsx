e865aaeae8e9609e9e4731b6f6dd993876ac9442
Zhoujia22 <827160082@qq.com>
2023-07-16 06:27:57 +0800

    处理登录成功和失败

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,16 +1,19 @@
 import type { FormEventHandler } from 'react'
+import type { AxiosError } from 'axios'
 import axios from 'axios'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { TopNav } from '../components/TopNav'
 import { useSignInStore } from '../stores/useSignInStore'
+import type { FormError } from '../lib/validate'
 import { hasError, validate } from '../lib/validate'
-import { ajax } from '../lib/ajax'
 import { Input } from '../components/Input'
+import { ajax } from '../lib/ajax'
 
 export const SignInPage: React.FC = () => {
   const { data, error, setData, setError } = useSignInStore()
-  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault()
     const newError = validate(data, [
       { key: 'email', type: 'required', message: '请输入邮箱地址' },
@@ -18,9 +21,17 @@ export const SignInPage: React.FC = () => {
       { key: 'code', type: 'required', message: '请输入验证码' },
       { key: 'code', type: 'length', min: 6, max: 6, message: '验证码必须是6个字符' },
     ])
+    const onSubmitError = (err: AxiosError<{ errors: FormError<typeof data> }>) => {
+      setError(err.response?.data?.errors ?? {})
+      throw error
+    }
     setError(newError)
     if (!hasError(newError)) {
-      ajax.post('/api/v1/session', data)
+      const response = await ajax.post<{ jwt: string }>('http://121.196.236.94:8080/api/v1/session', data)
+        .catch(onSubmitError)
+      const jwt = response.data.jwt
+      console.log('jwt', jwt)
+      localStorage.setItem('jwt', jwt)
     }
   }
 
@@ -47,7 +58,7 @@ export const SignInPage: React.FC = () => {
       <Input label='邮箱地址' placeholder='请输入邮箱，然后点击发送验证码' value={data.email}
         onChange={email => setData({ email })} error={error.email?.[0]} />
       <Input label='验证码' type='sms_code' placeholder='6位数字' value={data.code}
-        onChange={code => setData({ code })} error={error.code?.[0]} request={sendSmsCode}/>
+        onChange={code => setData({ code })} error={error.code?.[0]} request={sendSmsCode} />
       <div m-100px>
         <button j-btn type='submit'>登录</button>
       </div>
