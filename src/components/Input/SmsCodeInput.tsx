f7a2d1ab9e69f1fe3bd36d9d83c058482446ae0c
Zhoujia22 <827160082@qq.com>
2023-07-16 05:09:33 +0800

    优化倒计时功能

--- src/components/Input/SmsCodeInput.tsx
+++ src/components/Input/SmsCodeInput.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 type Props = {
   value?: string
@@ -7,35 +7,47 @@ type Props = {
   request?: () => Promise<unknown>
 }
 
+const maxCount = 60
 export const SmsCodeInput: React.FC<Props> = (props) => {
   const { value, placeholder, onChange, request } = props
-  const [started, setStarted] = useState(false)
-  const [count, setCount] = useState(5)
+  const [started, setStarted] = useState<Date>()
+  const [count, setCount] = useState(maxCount)
+  const timer = useRef<number>()
   const onClick = async () => {
     if (!request) { return }
     await request()
-    setStarted(true)
+    setStarted(new Date())
   }
 
+  const clearTimer = () => {
+    if (timer.current) {
+      window.clearInterval(timer.current)
+      timer.current = undefined
+    }
+  }
   useEffect(() => {
-    if (!started) { return }
-    if (count === -1) {
-      setStarted(false)
-      setCount(5)
+    if (!started) {
+      clearTimer()
       return
     }
-    const timer = setTimeout(() => {
-      setCount(count - 1)
-    }, 1000)
-    return () => clearTimeout(timer)
-  }, [started, count])
+    timer.current = window.setInterval(() => {
+      const seconds = Math.round((new Date().getTime() - started.getTime()) / 1000)
+      const count = maxCount - seconds
+      if (count < 0) {
+        setStarted(undefined)
+      }
+      setCount(count)
+    }
+    , 1000)
+    return clearTimer
+  }, [started])
 
   return (
     <div flex gap-x-16px>
       <input shrink-1 max-w="[calc(40%-8px)]" j-input-text type="text" placeholder={placeholder}
         value={value} onChange={e => onChange?.(e.target.value)} />
       {started
-        ? <button type="button" max-w="[calc(60%-8px)]" shrink-0 j-btn>{count}</button>
+        ? <button type="button" max-w="[calc(60%-8px)]" shrink-0 j-btn bg-coolgray text-light>{count}秒后可重新发送</button>
         : <button type="button" max-w="[calc(60%-8px)]" shrink-0 j-btn onClick={onClick}>发送验证码</button>
       }
     </div>
