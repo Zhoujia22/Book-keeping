83101f5bcd330f15af4c7c83eba47bbfa4a38bf8
Zhoujia22 <827160082@qq.com>
2023-07-27 11:14:02 +0800

    收集表单中的kind字段

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -4,6 +4,7 @@ import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { Tabs } from '../components/Tabs'
 import { TopNav } from '../components/TopNav'
+import { useCreateItemStore } from '../stores/useCreateItemStore'
 import s from './ItemsNewPage.module.scss'
 import { Tags } from './ItemNewPage/Tags'
 import { DateAndAmounts } from './ItemNewPage/DateAndAmount'
@@ -12,6 +13,8 @@ export const ItemsNewPage: React.FC = () => {
   const tabItems: { key: Item['kind']; text: string; element: ReactNode }[]
     = [{ key: 'expenses', text: '支出', element: <Tags kind='expenses' /> },
       { key: 'income', text: '收入', element: <Tags kind='income' /> }]
+
+  const { data, error, setData, setError } = useCreateItemStore()
 
   const [tabItem, setTabItem] = useState<Item['kind']>('expenses')
 
@@ -22,9 +25,10 @@ export const ItemsNewPage: React.FC = () => {
       </Gradient>
       <Tabs tabItems={tabItems}
         className="text-center grow-1 shrink-1 overflow-hidden"
-        value={tabItem}
+        value={data.kind!}
         classPrefix='itemsNewPage'
-        onChange={(item) => { setTabItem(item) }} />
+        onChange={(tabItem) => { setData({ kind: tabItem }) }} />
+      <div text-28px>{JSON.stringify(data)}</div>
       <DateAndAmounts className='grow-0 shrink-0' />
     </div>
   )
