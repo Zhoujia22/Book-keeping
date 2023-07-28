e309d285d3ecd8c8ad18da2bae68edcd1cb7c12d
Zhoujia22 <827160082@qq.com>
2023-07-29 01:14:48 +0800

    校验并提交表单数据（新建item表单）

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -4,6 +4,8 @@ import { Icon } from '../components/Icon'
 import { Tabs } from '../components/Tabs'
 import { TopNav } from '../components/TopNav'
 import { useCreateItemStore } from '../stores/useCreateItemStore'
+import { hasError, validate } from '../lib/validate'
+import { useAjax } from '../lib/ajax'
 import s from './ItemsNewPage.module.scss'
 import { Tags } from './ItemNewPage/Tags'
 import { ItemAmount } from './ItemNewPage/ItemAmount'
@@ -25,11 +27,24 @@ export const ItemsNewPage: React.FC = () => {
       element: <Tags kind='income'
         value={data.tag_ids} onChange={(ids) => setData({ tag_ids: ids })} />
     }]
-
-  const onSubmit = () => {
-    console.log('我是提交的按钮')
+  const { post } = useAjax({ showLoading: true, handleError: true })
+  const onSubmit = async () => {
+    const error = validate(data, [
+      { key: 'kind', type: 'required', message: '请选择类型：收入或支出' },
+      { key: 'tag_ids', type: 'required', message: '请选择一个标签' },
+      { key: 'happen_at', type: 'required', message: '请选择一个时间' },
+      { key: 'amount', type: 'required', message: '请输入金额' },
+      { key: 'amount', type: 'notEqual', value: 0, message: '金额不能为0' },
+    ])
+    setError(error)
+    if (hasError(error)) {
+      const message = Object.values(error).flat().join('\n')
+      window.alert(message)
+    } else {
+      const response = await post<Resource<Item>>('/api/v1/items', data)
+      console.log(response.data.resource)
+    }
   }
-
   return (
     <div className={s.wrapper} h-screen flex flex-col>
       <Gradient className='grow-0 shrink-0'>
@@ -40,13 +55,9 @@ export const ItemsNewPage: React.FC = () => {
         value={data.kind!}
         classPrefix='itemsNewPage'
         onChange={(tabItem) => { setData({ kind: tabItem }) }} />
-      <div text-28px>{JSON.stringify(data)}</div>
-      <ItemAmount className='grow-0 shrink-0' value={data.amount}
-        onChange={amount => setData({ amount })}
-        onSubmit={onSubmit}
-        itemDate={<ItemDate value={data.happen_at}
-          onChange={(happen_at) => setData({ happen_at })}
-        />} />
+      <ItemAmount className="grow-0 shrink-0" itemDate={
+        <ItemDate value={data.happen_at} onChange={(happen_at) => setData({ happen_at })} />
+      } value={data.amount} onChange={amount => setData({ amount })} onSubmit={onSubmit} />
     </div>
   )
 }
