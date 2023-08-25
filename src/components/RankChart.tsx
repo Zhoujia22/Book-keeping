34667a120e74758662d547e80aa8f1bda86b8f78
Zhoujia22 <827160082@qq.com>
2023-08-26 06:37:27 +0800

    修复RankChart金钱显示错误

--- src/components/RankChart.tsx
+++ src/components/RankChart.tsx
@@ -1,5 +1,3 @@
-import { Money } from './Money'
-
 type Props = {
   className?: string
   items?: { name: string; value: number | string; sign: string }[]
@@ -13,21 +11,25 @@ export const RankChart: React.FC<Props> = (props) => {
   const renderItems = () => {
     return (
       items?.map((item, index) =>
-        <div grid key={item.name} grid-cols="[48px_1fr_1fr]" grid-rows="[repeat(2,1fr)]"
+        <div key={item.name} grid grid-cols="[48px_1fr_1fr]" grid-rows="[repeat(2,1fr)]"
           text-12px items-center gap-y-6px gap-x-8px px-16px my-8px>
           <div row-start-1 col-start-1 row-end-3 col-end-2
             w-48px h-48px rounded-24px bg="#EFEFEF" flex justify-center items-center
             text-24px>{item.sign}</div>
-          <div row-start-1 col-start-2 row-end-2 col-end-3 self-end>{item.name} - {`${(parseFloat(item.value.toString()) / total * 100).toFixed(0)}%`}</div>
+          <div row-start-1 col-start-2 row-end-2 col-end-3 self-end>
+            {item.name} - {`${(parseFloat(item.value.toString()) / total * 100).toFixed(0)}%`}
+          </div>
           <div row-start-1 col-start-3 row-end-2 col-end-4 text-right self-end>
-            <Money value={parseFloat(item.value.toString())} /></div>
-          <div row-start-2 col-start-2 row-end-3 col-end-4 bg="#CCC" h-8px self-start relative overflow-hidden>
-            <div absolute h-full rounded-4px style={{ background: colors[index], width: `${parseFloat(item.value.toString()) / max * 100}%` }}></div>
+            {item.value} 元
+          </div>
+          <div row-start-2 col-start-2 row-end-3 col-end-4 h-8px self-start
+            rounded-4px overflow-hidden bg="#CCC" relative>
+            <div absolute h-full rounded-4px
+              style={{ background: colors[index], width: `${parseFloat(item.value.toString()) / max * 100}%` }} />
           </div>
         </div>)
     )
   }
-
   return (
     <div className={className}>
       {items?.[0]
